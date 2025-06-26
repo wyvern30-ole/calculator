b6b7267bacb48562bc8d706376105e806a62c6be
ole <lee21314159@gmail.com>
2025-06-25 20:01:50 -0500

    updated so inputs display in the textfield

--- src/Calculator.java
+++ src/Calculator.java
@@ -11,9 +11,11 @@ public class Calculator implements ActionListener {
 	// ui
 	JFrame frame;
 	JPanel panel;
+	GridBagConstraints constraints;
 
 	// textfield
-	JTextField tf;
+	JTextField textfield;
+	JTextField prevText;
 
 	// button groups
 	JButton numB[] = new JButton[10];
@@ -28,29 +30,38 @@ public class Calculator implements ActionListener {
 	JButton prevAnsB, delB, clearB;
 
 	// extra variables
-	private int prevAns = 0; // previous answer so user doesn't have to manually type it again
+	private String currentText = "";
+	private String prevAns = "0"; // previous answer so user doesn't have to manually type it again
 	Font font = new Font("Monospaced", Font.BOLD, 30); // font for all text
 
+	/**
+	 * Creates Calculator object, which will look/act like a handheld calculator
+	 */
 	public Calculator() {
 		// creating frame
 		frame = new JFrame("Calculator");
 
-		// setting up panel
-		// panel = new JPanel();
-		// panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
-		// panel.setLayout(new GridLayout(0,1));
-
 		// setting up frame
-		frame.setSize(400, 550);
-		// frame.add(panel, BorderLayout.CENTER);
+		frame.setSize(400, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
-		frame.setLayout(null);
+		frame.setLayout(new BorderLayout());
 
-		// making textfield
-		tf = new JTextField();
-		tf.setBounds(20, 20, 350, 50);
-		tf.setFont(font);
+		// setting up panel and grid
+		panel = new JPanel(new GridBagLayout());
+		// setting up constraints
+		constraints = new GridBagConstraints();
+		constraints.insets = new Insets(5, 5, 5, 5); // space around components
+		constraints.fill = GridBagConstraints.BOTH; // stretch to fill cell
+
+		// making main textfield
+		textfield = new JTextField();
+		textfield.setFont(font);
+
+		// making textfield which displays previous answer
+		prevText = new JTextField();
+		prevText.setFont(font);
+		prevText.setEditable(false);
 
 		// making buttons
 
@@ -68,7 +79,7 @@ public class Calculator implements ActionListener {
 		multiplyB = new JButton("x");
 		divideB = new JButton("/");
 		powerB = new JButton("^");
-		rootB = new JButton("√‾");
+		rootB = new JButton("√");
 
 		// extra arithmetic
 		decimalB = new JButton(".");
@@ -77,7 +88,7 @@ public class Calculator implements ActionListener {
 		equalsB = new JButton("=");
 
 		// extra function
-		prevAnsB = new JButton("Previous");
+		prevAnsB = new JButton("Prev Ans");
 		delB = new JButton("Delete");
 		clearB = new JButton("Clear");
 
@@ -94,9 +105,9 @@ public class Calculator implements ActionListener {
 		extraArithB[2] = closeParenB;
 		extraArithB[3] = equalsB;
 
-		extraFunctionB[0] = prevAnsB;
-		extraFunctionB[1] = delB;
-		extraFunctionB[2] = clearB;
+		extraFunctionB[0] = clearB;
+		extraFunctionB[1] = prevAnsB;
+		extraFunctionB[2] = delB;
 
 		// setting up buttons functionality/look
 		for (int i = 0; i < 6; i++) {
@@ -115,21 +126,120 @@ public class Calculator implements ActionListener {
 			extraFunctionB[i].setFocusable(false);
 		}
 
+		// adding textfields to panel
+		// main textfield
+		constraints.gridwidth = 5;
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		panel.add(textfield, constraints);
+		// previous answer textfield
+		constraints.gridwidth = 3;
+		constraints.gridy = 1;
+		panel.add(prevText, constraints);
+
+		// adding extra function buttons
+		constraints.gridwidth = 2;
+		addItem(3, 1, extraFunctionB[0]);
+		addItem(3, 2, extraFunctionB[2]);
+		constraints.gridwidth = 3;
+		addItem(0, 2, extraFunctionB[1]);
+		constraints.gridwidth = 1;
+
+		// adding number buttons
+		addItem(0, 6, numB[0]);
+		addItem(0, 5, numB[1]);
+		addItem(1, 5, numB[2]);
+		addItem(2, 5, numB[3]);
+		addItem(0, 4, numB[4]);
+		addItem(1, 4, numB[5]);
+		addItem(2, 4, numB[6]);
+		addItem(0, 3, numB[7]);
+		addItem(1, 3, numB[8]);
+		addItem(2, 3, numB[9]);
+
+		// adding operator buttons
+		addItem(3, 5, operatorB[0]);
+		addItem(4, 5, operatorB[1]);
+		addItem(3, 4, operatorB[2]);
+		addItem(4, 4, operatorB[3]);
+		addItem(3, 3, operatorB[4]);
+		addItem(4, 3, operatorB[5]);
+
+		// adding extra arithmetic buttons
+		addItem(1, 6, extraArithB[0]);
+		addItem(2, 6, extraArithB[1]);
+		addItem(3, 6, extraArithB[2]);
+		addItem(4, 6, extraArithB[3]);
+
 		// end of setup
-		frame.add(tf);
+		frame.add(panel);
 		frame.setVisible(true);
 	}
 
+	/**
+	 * Begins the program
+	 * 
+	 * @param args - automatically passed argument
+	 */
 	public static void main(String[] args) {
 		System.out.println("Running...");
-		// System.out.println(Math.pow(9, 1.0 / 2));
 		new Calculator();
 	}
 
+	/**
+	 * Adds a JButton to a specified x and y and given width inside the frame
+	 * 
+	 * @param width - number of spaces horizontally button takes up
+	 * @param x     - horizontal position of item
+	 * @param y     - vertical position of item
+	 * @param item  - button to add
+	 */
+	public void addItem(int x, int y, JButton item) {
+		constraints.gridx = x;
+		constraints.gridy = y;
+		panel.add(item, constraints);
+	}
+
+	/**
+	 * Handles action events triggered by user interaction. Reacts accordingly when
+	 * buttons are clicked
+	 * 
+	 * @param e - ActionEvent triggered by a user interaction
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		// clear button is pressed
+		if (e.getSource() == extraFunctionB[0]) {
+			textfield.setText("");
+		}
+		// previous answer button is pressed
+		else if (e.getSource() == extraFunctionB[1]) {
+			textfield.replaceSelection(prevAns);
+		}
+		// delete button is pressed
+		else if (e.getSource() == extraFunctionB[2]) {
+			textfield.setText(currentText.substring(0, currentText.length() - 1));
+		}
 
+		// arithmetic/symbol button is pressed
+		else if (e.getSource() != extraArithB[3]) {
+			textfield.replaceSelection(((JButton) e.getSource()).getText());
+		}
+		// equals button is pressed
+		else if (e.getSource() == extraArithB[3]) {
+			// TODO
+		}
+
+		// number button is pressed
+		else {
+			for (int i = 0; i < 10; i++) {
+				if (e.getSource() == numB[i]) {
+					textfield.replaceSelection(String.valueOf(i));
+				}
+			}
+		}
+
+		currentText = textfield.getText();
 	}
 
 }
